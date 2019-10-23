5b65dcb9cd89e2331fa161641fd7acaf1a285d82
one_true_ <piranin@soccer.tnc.ne.jp>
2019-10-23 17:57:22 +0900

    natonatonatto

--- bot.rb
+++ bot.rb
@@ -11,6 +11,7 @@
 #####
 
 require 'discordrb'
+require 'natto'
 require 'net/http'
 require 'uri'
 require 'json'
---
+++ src/commands/natto.rb
@@ -0,0 +1,27 @@
+module Bot::DiscordCommands
+  module Nattotext
+    extend Discordrb::Commands::CommandContainer
+
+    attrs = {
+      description: '形態素解析するマン',
+      usage: 'natto <words>'
+    }
+
+    command :natto, attrs do |event, *words|
+      words = words.join(' ').to_s
+      event.respond(natto_message(words: words))
+    end
+
+    def self.natto_message(words: nil)
+      message = ""
+      if words == nil then
+        message = "解析したい文字列を入力してくだしあ\n"
+      else
+        nm = Natto::MeCab.new
+        nm.parse(words) do |n|
+          message += "#{n.surface}\t#{n.feature}\n"
+        end
+      end
+    end
+  end
+end
