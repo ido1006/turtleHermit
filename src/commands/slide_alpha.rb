ce33c63023247e87c25f772cd25809e50edb205d
Hibiki <43987522+HimHim1113@users.noreply.github.com>
2020-04-07 21:06:14 +0900

    引数なしでも動くぞ（動作未確認）

---
+++ src/commands/slide_alpha.rb
@@ -0,0 +1,37 @@
+module Bot::DiscordCommands
+    module Slide
+      extend Discordrb::Commands::CommandContainer
+      
+      attrs = {
+        description: 'アルファベットを指定した分ずらします', # プログラムの説明を書きます ,を忘れないで(2敗)
+        usage: 'slide <num> <text>' # コードの使い方を書きます
+      }
+  
+      command :slide, attrs do |event, num, args| # event はコマンド自身、args は引数
+        event.respond(slide_message(num: num, args: args)) #respondの中身のmessageをsample_message から受け取りますよってこと
+      end
+  
+      def self.slide_message(num: nil, args: nil) # self. を忘れずに！
+        num ||= 0
+        args ||= "abc" # 引数が空っぽの時の処理
+        alphabet_small = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        alphabet_big = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+        for i in 0..25 do
+            j = i + num
+            if j < 0 then
+                j += 26 * (-j / 26 + 1)
+            end
+            if j >= 26 then
+                j -= 26 * (j / 26)
+            end
+
+            args = args.gsub(alphabet_small[i], alphabet_small[j])
+            args = args.gsub(alphabet_big[i], alphabet_big[j])
+        end
+
+        message = "#{args}"
+        message # 戻り値としてmessageを返します
+      end
+    end
+  end
