9aedf8db5dcd98113b72268174df4563046e9312
kKanai_ <piranin@soccer.tnc.ne.jp>
2021-07-06 21:12:03 +0900

    changed poyo

--- src/events/poyo.rb
+++ src/events/poyo.rb
@@ -3,9 +3,16 @@ module Bot::DiscordEvents
     extend Discordrb::EventContainer
 
     message do |event|
-      if event.user.server.name == "ぽよ"
+      if event.channel.id == 861905463871668244
         str = event.message.content
-        if str.match(/[^ぽよー〜ぅん]/)
+        if !(str.include?("ぽ") ||
+            str.include?("よ") ||
+            str.include?("ぅ") ||
+            str.include?("〜") ||
+            str.include?("ー") ||
+            str.include?("ん") ||
+            str.include?("！") ||
+            str.include?("？") )
           event.message.delete
         end
       end
