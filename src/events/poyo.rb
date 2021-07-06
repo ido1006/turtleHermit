8012560c1c2895baedcc13d82516467c3f30f713
kKanai_ <piranin@soccer.tnc.ne.jp>
2021-07-06 22:34:14 +0900

    bugfixfixbug

--- src/events/poyo.rb
+++ src/events/poyo.rb
@@ -3,18 +3,18 @@ module Bot::DiscordEvents
     extend Discordrb::EventContainer
 
     message do |event|
-      poyo=["ぽ","よ","ぅ","ん","ー","！","？","〜"]
+      poyo=["ぽ","よ","ぅ","ん","ー","！","？","〜","…"]
       message=event.message.content
-      if poyo.any? { |i| message.include?(i) } && event.channel.id == 861905463871668244 then
-        for i in poyo do
-          message.delete!(i)
+      if event.channel.id == 861905463871668244 then
+        if poyo.any? { |i| message.include?(i) } then
+          for i in poyo do
+            message.delete!(i)
+          end
+          if message=="" then
+          else
+            event.message.delete
+          end
         end
-        if message=="" then
-        else
-          event.message.delete
-        end
-      else
-        event.message.delete
       end
     end
   end
