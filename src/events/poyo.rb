0b0c3a1c3edb8b7a87ab9559cc9cc30be1eccd1b
kKanai_ <piranin@soccer.tnc.ne.jp>
2021-07-06 22:38:51 +0900

    謎バグ修正になるといいが

--- src/events/poyo.rb
+++ src/events/poyo.rb
@@ -3,7 +3,7 @@ module Bot::DiscordEvents
     extend Discordrb::EventContainer
 
     message do |event|
-      poyo=["ぽ","よ","ぅ","ん","ー","！","？","〜","…"]
+      poyo=["ぽ","よ","ぅ","ん","ー","！","？","〜"]
       message=event.message.content
       if event.channel.id == 861905463871668244 then
         if poyo.any? { |i| message.include?(i) } then
