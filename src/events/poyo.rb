6f7ad9e947ed5a0cbd27e6b4a442d4271dc6e157
kKanai_ <piranin@soccer.tnc.ne.jp>
2021-07-06 21:34:46 +0900

    fix poyo

--- src/events/poyo.rb
+++ src/events/poyo.rb
@@ -5,8 +5,9 @@ module Bot::DiscordEvents
     message do |event|
       if event.channel.id == 861905463871668244
         str = event.message.content
-        for s in str do:
+        for s in event.message.content do:
           if (s == 'ぽ') || (s == 'よ') || (s == 'ぅ') || (s == 'ょ') || (s == 'ー') || (s == '〜') || (s == '！') || (s == '？')
+            event.send_message("Good poyo...")
           else
             event.message.delete
           end
