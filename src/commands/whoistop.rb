926c1dc41348627f53ca19e7b492d748df308b19
YTJVDCM <yamaaaaaatai@gmail.com>
2021-08-07 00:36:05 +0900

    Added whoistop command

---
+++ src/commands/whoistop.rb
@@ -0,0 +1,22 @@
+module Bot::DiscordCommands
+    module Whoistop
+        extend Discordrb::Commands::CommandContainer
+
+        attrs = {
+            description: 'かめせんにんがランダムに部長を決めます。\n引数にmentionをつけるとメンションが飛びます。',
+            usage: 'whoistop (mention)'
+        }
+
+        command :whoistop, attrs do |event, args|
+            if args=="mention" then
+                members = event.server.members.map(&:id)
+                result = members.sample(3)
+                event.send_message("​　部長:\t<@#{result[0]}>\n副部長:\t<@#{result[1]}>\n　会計:\t<@#{result[2]}>");
+            else
+                members = event.server.members.map(&:display_name)
+                result = members.sample(3)
+                event.send_message("​　部長:\t#{result[0]}\n副部長:\t#{result[1]}\n　会計:\t#{result[2]}");
+            end
+        end
+    end
+end
